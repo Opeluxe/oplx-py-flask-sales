edeb2f3ff46f35c91f9980147f5c463476afe5f0
Opeluxe <opeluxe@icloud.com>
2019-11-25 20:22:13 -0500

    update flask api

---
+++ README.md
@@ -0,0 +1,11 @@
+# oplx-py-streamlit-sales
+Basic implementation of web service (using **flask**) to consume ML model (sales prediction).
+
+## Execution
+Command line (local): `flask run` (will execute app.py and locally activate web service at http://127.0.0.1:5000/api)  
+Heroku: configuration set in `Procfile`. Web service can be tested pointing to web service using [https://oplx-py-flask-sales.herokuapp.com/api](https://oplx-py-flask-sales.herokuapp.com/api).
+
+## Files information
+- `sales_build_and_train.py` contains a simple generation of ML model (using sci-kit learn pipeline and pickle serialization).
+- `app.py` is a basic example of web service for ML model consumption (use of **flask** functionality to receive data selection parameters and return dataframe information, both using JSON via POST request).
+- `requirements.txt`, `runtime.txt` and `Procfile` are used during web service execution (Heroku).
--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 
 
 import pandas as pd
-#import numpy as np
+import numpy as np
 import dill as pickle
 import flask
 from flask import request
@@ -16,16 +16,37 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Constants for layout and dataframe manipulation
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + '/data/test_k1.csv'
+DATA_FULL_PATH = os.path.dirname(os.path.abspath(__file__)) + '/data/test_k.csv'
+DATA_SAMPLE_PATH = os.path.dirname(os.path.abspath(__file__)) + '/data/test_ks.csv'
 MODEL_PATH = os.path.dirname(os.path.abspath(__file__)) + '/model/model_v3.pk'
 FEATURES = ['Store', 'DayOfWeek', 'Date', 'Customers', 'Open', 'Promo', 'StateHoliday', 'SchoolHoliday']
+DATA_SORT = ['Date', 'Store']
 
 # Data manipulation routines
-def load_data(nrows):
-    loaded_data = pd.read_csv(DATA_PATH, sep=',', error_bad_lines=False, low_memory=False, nrows=nrows)
-    loaded_data = loaded_data[FEATURES]
-    return loaded_data
+def load_data(nrows, sample, random):
+    loaded_data = pd.read_csv(DATA_SAMPLE_PATH if sample else DATA_FULL_PATH, 
+                              sep=',', 
+                              error_bad_lines=False, 
+                              low_memory=False)
+    loaded_data = select_data(loaded_data, nrows, random)
+    loaded_data.sort_values(by=DATA_SORT, inplace=True)
+    return loaded_data[FEATURES]
     
+def select_data(data, nrows, rndm):
+    total_rows = len(data)
+    if total_rows > nrows:
+        drop_rows = total_rows - nrows
+        if rndm == False:
+            drop_indx = np.linspace(start=nrows, stop=total_rows - 1, 
+                                    num=drop_rows, dtype=int)
+        else:
+            drop_indx = np.sort(np.random.choice(total_rows - 1, drop_rows, replace=False))
+        selected_data = data.drop(drop_indx)
+        selected_data.reset_index(inplace = True)
+    else:
+        selected_data = data
+    return selected_data
+
 def load_model():
     pickle._dill._reverse_typemap['ClassType'] = type
     with open(MODEL_PATH, 'rb') as model:
@@ -46,10 +67,12 @@ app = flask.Flask(__name__)
 def index():
     #getting an array of features from the post request's body
     nrows = request.get_json()['number']
+    sample = request.get_json()['sample']
+    random = request.get_json()['random']
     
     #predicting using model
     loaded_model = load_model()
-    loaded_data = load_data(nrows=nrows)
+    loaded_data = load_data(nrows=nrows, sample=sample, random=random)
     predicted_values, selected_data = predict_data(loaded_data, loaded_model)
 
     #returning the response object as json
